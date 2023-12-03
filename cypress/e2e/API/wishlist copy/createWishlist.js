accb9624bc5775f8d3d58fc67ca8728e030cbed5
DubovikovArtem <dubovikov.arm@gmail.com>
2023-12-03 11:44:01 +0200

    add folder

--- cypress/e2e/API/wishlist copy/createWishlist.js
+++ cypress/e2e/API/wishlist copy/createWishlist.js
@@ -4,7 +4,7 @@ export function addToWish() {
         url: `https://bookcart.azurewebsites.net/api/Wishlist/ToggleWishlist/21284/2`,
         headers: {
             'Accept': 'text/plain',
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ4cXdlQVMxMjEyMyIsInVzZXJpZCI6IjIxMjg0IiwidXNlclR5cGVJZCI6IjIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiIyIiwianRpIjoiOGVlYTI5YTAtM2MwNi00ZjA2LWFkYTAtNTQ5Y2MzZjE5YWU3IiwiZXhwIjoxNzAxNTk2NTExLCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo0NDM2NC8iLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdDo0NDM2NC8ifQ.WgTQto5DFpUGnX8wYGt2Aha24ktzPk-zOTucoGEIQuo'
+            'Authorization': 'Bearer '
         },
     }).then((response) => {
         expect(response.status).to.eq(200);
@@ -20,7 +20,7 @@ export function addToWish() {
 //         headers: {
 //             'Accept': 'text/plain',
 //             'Authorization': `bearer ${token}`
-//             //'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ4cXdlQVMxMjEyMyIsInVzZXJpZCI6IjIxMjg0IiwidXNlclR5cGVJZCI6IjIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiIyIiwianRpIjoiOGVlYTI5YTAtM2MwNi00ZjA2LWFkYTAtNTQ5Y2MzZjE5YWU3IiwiZXhwIjoxNzAxNTk2NTExLCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo0NDM2NC8iLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdDo0NDM2NC8ifQ.WgTQto5DFpUGnX8wYGt2Aha24ktzPk-zOTucoGEIQuo'
+//             //'Authorization': 'Bearer '
 //         },
 //     }).then((response) => {
 //         expect(response.status).to.eq(200);
